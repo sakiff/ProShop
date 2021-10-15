c3ad0552f8115d0a2b3f51ee2f0c34de1a6f35e4
Sakif Chowdhury <sakif@Sakifs-MacBook-Pro.local>
2021-10-14 20:05:17 -0400

    reinitialized git repo

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import products from '../products'
+import {Row, Col} from 'react-bootstrap'
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+    return (
+        <>
+            <h3 className="mp-3">Latest Products</h3>
+            <Row>
+                {products.map(product => (
+                  <Col sm={12} md={6} lg={4}>
+                    <Product product={product}/>
+                  </Col>  
+                ))}
+            </Row>
+        </>
+    )
+}
+
+export default HomeScreen
